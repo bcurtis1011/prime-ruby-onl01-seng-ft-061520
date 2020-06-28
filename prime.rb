90f364d17fb70680d446f4726804a654f3ff006b
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-28 18:24:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 def prime?(number)
   test = (2..number-1).all? {|num| number%num != 0}
-  if test == number.negative?() || number == 0
+  if test == number.negative?()
+    return false
+  elsif number == 0
+    return false
+  elsif number == 1
     return false
   else
     return test
